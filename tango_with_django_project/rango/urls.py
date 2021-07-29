cbc2d60f84c55fd7ffd36b96c78c797acebf585e
2593004 <695140409@qq.com>
2021-07-30 00:07:04 +0100

    chapter8 test done

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 29 14:06:42 2021
-
-@author: YULiu
-"""
 from django.urls import path
 from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
