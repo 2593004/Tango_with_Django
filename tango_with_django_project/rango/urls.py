c5f2a5e9f804d5de438fddcfe5f2de1af5810f2d
2593004 <695140409@qq.com>
2021-07-29 16:38:16 +0100

    chapter4 done

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -6,8 +6,11 @@ Created on Thu Jul 29 14:06:42 2021
 """
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
 path('about/', views.about, name='about'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
